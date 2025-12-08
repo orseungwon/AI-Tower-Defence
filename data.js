0c0dabd2ca4b78b0f00cbb0259b732964d63e25a
orseungwon <youseungwon1013@gmail.com>
2025-12-08 20:27:22 +0900

    AI prompt update

--- data.js
+++ data.js
@@ -223,9 +223,9 @@ const aiPopulationPositions = [
 ];
 
 const aiTurretPositions = [
-  {id: 1, gx: 12, gy: 3},
+  {id: 1, gx: 12, gy: 5},
   {id: 2, gx: 11, gy: 5},
-  {id: 3, gx: 12, gy: 5},
+  {id: 3, gx: 12, gy: 3},
   {id: 4, gx: 9, gy: 6},
   {id: 5, gx: 8, gy: 4},
   {id: 6, gx: 11, gy: 6},
